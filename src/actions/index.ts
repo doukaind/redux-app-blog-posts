3ced90e0341881a7ae246c60e897cad964dab11c
Dawid Skomorochow <dawid.skomorochow.job@gmail.com>
2022-02-18 20:42:43 +0100

    add loadash to memoize

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,11 +4,11 @@ export const fetchPosts = () => async (dispatch: any) => {
   // Bad approach!
   const response = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const selectPost = () => {
-  return {
-    type: "SELECT_POST",
-  };
+export const fetchUser = (id: string | number) => async (dispatch: any) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
