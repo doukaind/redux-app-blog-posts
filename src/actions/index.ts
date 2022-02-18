201dada98bb5b43597d804841ad830f698c9a5c3
Dawid Skomorochow <dawid.skomorochow.job@gmail.com>
2022-02-18 21:44:18 +0100

    add two ways of fetching data with memoizing

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,14 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers =
+  () => async (dispatch: any, getState: any) => {
+    // console.log(dispatch);
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, "userId"));
+    console.log("USER IDS: ", userIds);
+    userIds.forEach((id) => dispatch(fetchUser(id))); // we don't have to put here await keyword, because we don't have other code
+  };
 
 export const fetchPosts = () => async (dispatch: any) => {
   // Bad approach!
@@ -6,6 +16,15 @@ export const fetchPosts = () => async (dispatch: any) => {
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+// export const fetchUser = (id: string | number) => async (dispatch: any) =>
+//   _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
 
 export const fetchUser = (id: string | number) => async (dispatch: any) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
